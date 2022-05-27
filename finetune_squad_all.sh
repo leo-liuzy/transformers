8a507a49bd9fe9161ddbf1ba539b5391bd54dcdf
Leo Liu <zeyuliu@devfair0253.h2.fair>
2022-05-26 22:27:07 -0700

    update run script

--- finetune_squad_all.sh
+++ finetune_squad_all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sbatch finetune_squad_seq-len_0_1_0_9.slurm
+# sbatch finetune_squad_seq-len_0_1_0_9.slurm
 # sbatch finetune_squad_mp0_15.slurm
-sbatch finetune_squad_mp0_5.slurm
+# sbatch finetune_squad_mp0_5.slurm
+# sbatch finetune_squad_mp0_5.slurm
+sbatch finetune_squad_seq-len.slurm
