265f41bae6486fdc617b3a6d11f15637bfed1019
Leo Liu <zeyuliu@devfair0253.h2.fair>
2022-05-28 13:11:28 -0700

    add script to finetune 0.2-0.8

--- finetune_glue_all.sh
+++ finetune_glue_all.sh
@@ -7,5 +7,6 @@ do
     # sbatch finetune_glue_mp0_4.slurm
     # sbatch finetune_glue_mp0_5.slurm
     # sbatch finetune_glue_seq-len_0_1_0_9.slurm
-    sbatch finetune_glue_seq-len.slurm
+    sbatch finetune_glue_seq-len_0_2_0_8.slurm
+    # sbatch finetune_glue_seq-len.slurm
 done
